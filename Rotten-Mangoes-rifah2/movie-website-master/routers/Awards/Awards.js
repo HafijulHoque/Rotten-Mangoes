36f91bd51236e5588e8143b57b47d89515c1e955
Rifah <1905019@ugrad.cse.buet.ac.bd>
2022-08-31 01:25:34 +0600

    31Aug_1.25am

--- Rotten-Mangoes-rifah2/movie-website-master/routers/Awards/Awards.js
+++ Rotten-Mangoes-rifah2/movie-website-master/routers/Awards/Awards.js
@@ -3,15 +3,20 @@ const DB_movie = require('../../DB_codes/DB_movie')
 const DB_review = require('../../DB_codes/DB_review')
 const DB_watchlist = require('../../DB_codes/DB_watchlist')
 const DB_rating = require('../../DB_codes/DB_rating')
+const DB_awards = require('../../DB_codes/DB_awards')
 const router = express.Router({ mergeParams: true })
-router.get('/',(req,res)=>
-    {
+router.get('/',async (req, res) => {
+        const awards = await DB_awards.getAllAwards();
         console.log("from awards get request");
-        const data=
+        console.log(awards)
+        const data =
             {
-
+                pageTitle: 'Awards',
+                isAuth: req.session.isAuth,
+                username: req.session.username,
+                awards:awards,
             }
-        res.render('Awards',data)
+        res.render('Awards', data)
 
     }
 )
